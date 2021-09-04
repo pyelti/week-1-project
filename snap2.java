60fc948dd6c9c8984b3484212e2a514334ff6723
pyelti <90082109+pyelti@users.noreply.github.com>
2021-09-04 00:11:17 -0500

    Add files via upload

---
+++ snap2.java
@@ -0,0 +1,20 @@
+ublic class Fans {
+private String Name;
+private SnapchatAccount t = new snapchatAccount();
+
+public Fans(String name) {
+super();
+Name = name;
+
+}
+
+public void update()
+{
+
+System.out.println("Hey " +Name+ ", BTS Uploaded a new Song.");
+}
+
+public void FollowBeautyPage(snapchatAccount TA) {
+t = TA;
+}
+}
